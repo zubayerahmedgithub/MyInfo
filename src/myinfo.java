78c7167c5dbcdfb7584bd4280826694ad071e2d8
zubayerahmedgithub <sajib.cu.apece06@gmail.com>
2024-03-14 12:42:58 -0400

    Initial commit

---
+++ src/myinfo.java
@@ -0,0 +1,8 @@
+public class myinfo {
+    public static void main(String[] args) {
+        // My First Name And Last Name
+        String X = "Zubayer Ahmed";
+        String Y = "Sajib";
+        System.out.println("Hello I am " + X  + " " + Y + " " + " I am enrolled in Java training ");
+    }
+}
